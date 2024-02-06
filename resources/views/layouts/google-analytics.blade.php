0f86535497b3d71999d83560a5eef79c8d8e78a4
ifs20035-itdel <ifs20035@students.del.ac.id>
2024-02-06 10:42:40 +0700

    sync google analytics with tag in environment variable

--- resources/views/layouts/google-analytics.blade.php
+++ resources/views/layouts/google-analytics.blade.php
@@ -1,8 +1,7 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-SPEDD24VWJ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-SPEDD24VWJ');
-</script>
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.ga4.measurementId') }}"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '{{ config('services.ga4.measurementId') }}');
+  </script>
